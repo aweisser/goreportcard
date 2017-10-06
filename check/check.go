c90edb51f661916d64d73a7175317f97973bcb19
aweisser <armin.weisser@gmail.com>
2017-10-06 18:46:39 +0000

    Domain model should not be hidden in utils.go. I'd like to see the model alltogether in one place for better readability.

--- check/check.go
+++ check/check.go
@@ -1,5 +1,10 @@
 package check
 
+import (
+	"strconv"
+	"strings"
+)
+
 // Check describes what methods various checks (gofmt, go lint, etc.)
 // should implement
 type Check interface {
@@ -9,4 +14,37 @@ type Check interface {
 	// Percentage returns the passing percentage of the check,
 	// as well as a map of filename to output
 	Percentage() (float64, []FileSummary, error)
+}
+
+// Error contains the line number and the reason for
+// an error output from a command
+type Error struct {
+	LineNumber  int    `json:"line_number"`
+	ErrorString string `json:"error_string"`
+}
+
+// FileSummary contains the filename, location of the file
+// on GitHub, and all of the errors related to the file
+type FileSummary struct {
+	Filename string  `json:"filename"`
+	FileURL  string  `json:"file_url"`
+	Errors   []Error `json:"errors"`
+}
+
+// AddError adds an Error to FileSummary
+func (fs *FileSummary) AddError(out string) error {
+	s := strings.SplitN(out, ":", 2)
+	msg := strings.SplitAfterN(s[1], ":", 3)[2]
+
+	e := Error{ErrorString: msg}
+	ls := strings.Split(s[1], ":")
+	ln, err := strconv.Atoi(ls[0])
+	if err != nil {
+		return err
+	}
+	e.LineNumber = ln
+
+	fs.Errors = append(fs.Errors, e)
+
+	return nil
 }
